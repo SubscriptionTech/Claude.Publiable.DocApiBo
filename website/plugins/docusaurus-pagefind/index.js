da46a175ba84bf4032c2f91535a1f3e990bbc559
ChezMose <chezmose@gmail.com>
2026-06-24 17:41:54 +0200

    chore: update shared/DocApi submodule, sync pagefind plugin

--- website/plugins/docusaurus-pagefind/index.js
+++ website/plugins/docusaurus-pagefind/index.js
@@ -1,13 +1,13 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = function pagefindPlugin() {
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default function pagefindPlugin(_context, _options) {
   return {
     name: 'docusaurus-pagefind',
     getThemePath() {
-      return path.resolve(__dirname, 'theme');
-    },
-    getTypeScriptThemePath() {
-      return path.resolve(__dirname, 'theme');
+      return path.resolve(__dirname, './theme');
     },
   };
-};
+}
